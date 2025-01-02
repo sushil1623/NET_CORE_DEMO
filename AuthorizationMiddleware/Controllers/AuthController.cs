268b37379c5ddb50f89d2f29c3a838c8e749980c
sushil kumar <sushilkumar.kumar29@gmail.com>
2025-01-03 01:40:36 +0530

    Authorization Middleware (RequireRole & RequireClaim)

---
+++ AuthorizationMiddleware/Controllers/AuthController.cs
@@ -0,0 +1,144 @@
+﻿using DemoAPI.DAL.Models;
+using DemoAPI.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthorizationMiddleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(RefreshTokenContext context, IConfiguration configuration) : ControllerBase
+    {
+        private RefreshTokenContext _context = context;
+        private IConfiguration _configuration = configuration;
+
+
+        [HttpPost]
+        [Route("AddUser")]
+        public IActionResult AddUser([FromBody] UserModel user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok("User has been created successfully");
+        }
+
+        [HttpPost]
+        [Route("Login")]
+        public IActionResult Login([FromBody] UserAuthenticateModel user)
+        {
+            var userDetails = _context.Users.Where(x => x.UserId == user.UserId
+            && x.Password == user.Password).FirstOrDefault();
+            if (userDetails is not null)
+            {
+
+                return Ok(new LoginResponse
+                {
+                    AccessToken = GetAccessToken(userDetails),
+                    RefreshToken = GetRefreshToken(userDetails)
+                });
+            }
+            else
+            {
+                return BadRequest("User id or password is invalid");
+            }
+        }
+
+        [HttpPost]
+        [Route("RefreshToken")]
+        public IActionResult RefreshToken([FromBody] LoginResponse refreshToken)
+        {
+            var refreshTokens = _context.RefreshTokens.Where(x => x.RefreshToken == refreshToken.RefreshToken).FirstOrDefault();
+            if (refreshTokens is not null)
+            {
+                var userDetails = _context.Users.Where(x => x.UserId == refreshTokens.UserId).FirstOrDefault();
+                if (userDetails is not null)
+                {
+
+                    return Ok(new LoginResponse
+                    {
+                        AccessToken = GetAccessToken(userDetails),
+                        RefreshToken = GetRefreshToken(userDetails)
+                    });
+                }
+                else
+                {
+                    return BadRequest("User id or password is invalid");
+                }
+            }
+            else
+            {
+                return BadRequest("Refresh Token is not valid");
+            }
+        }
+
+        private string GetRefreshToken(UserModel user)
+        {
+            var randomBytes = new byte[32];
+            using (var rng = System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes);
+                string refreshToken = Convert.ToBase64String(randomBytes);
+                _context.RefreshTokens.Add(new RefreshTokenModel
+                {
+                    RefreshToken = refreshToken,
+                    UserId = user.UserId
+                });
+                _context.SaveChanges();
+                return refreshToken;
+            }
+        }
+
+        private string GetAccessToken(UserModel userDetails)
+        {
+            var jwtSecurity = new JwtSecurityTokenHandler();
+            var securityToken = jwtSecurity.CreateToken(new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(
+                [
+                    new(ClaimTypes.Email,userDetails.EmailId.ToString()),
+                    new(ClaimTypes.NameIdentifier,userDetails.UserId.ToString()),
+                    new(ClaimTypes.Role,userDetails.UserRole)
+                ]),
+                Issuer = _configuration["JWTSettings:Issuer"],
+                Audience = _configuration["JWTSettings:Audience"],
+                IssuedAt = DateTime.UtcNow,
+                Expires = DateTime.UtcNow.AddMinutes(1),
+                SigningCredentials = new SigningCredentials(
+                     new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWTSettings:Key"])), SecurityAlgorithms.HmacSha512),
+            });
+            string accessToken = jwtSecurity.WriteToken(securityToken);
+            return accessToken;
+        }
+
+        [HttpPost]
+        [Authorize(Policy = "OnlyAdmin")]
+        [Route("AddProduct")]
+        public IActionResult AddProduct()
+        {
+            var products = new List<ProductModel>()
+            {
+                new ProductModel{ ProductId=1, ProductName="Product 1", ProductDescription="Product Description 1"},
+                new ProductModel{ ProductId=2, ProductName="Product 2", ProductDescription="Product Description 2"},
+                new ProductModel{ ProductId=3, ProductName="Product 3", ProductDescription="Product Description 3"},
+            };
+            _context.Products.AddRange(products);
+            _context.SaveChanges();
+            return Ok("Product has been addedd successfully");
+        }
+
+        [HttpGet]
+        [Authorize(Policy = "AdminOrUser")]
+        [Route("GetProducts")]
+        public IActionResult GetProduct()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+    }
+}
---
+++ AuthorizationMiddleware/Program.cs
@@ -0,0 +1,70 @@
+using DemoAPI.DAL;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    option.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(option =>
+{
+    option.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+    {
+
+        ValidIssuer = builder.Configuration["JWTSettings:Issuer"],
+        ValidAudience = builder.Configuration["JWTSettings:Audience"],
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWTSettings:Key"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+builder.Services.AddAuthorization(option =>
+{
+    option.AddPolicy("OnlyAdmin", build =>
+    {
+        build.RequireRole("Admin");
+        build.RequireClaim(ClaimTypes.Email, "sushil01@yopmail.com");
+    });
+    option.AddPolicy("AdminOrUser", build =>
+    {
+        build.RequireRole(new string[] {"Admin","User"});
+    });
+    option.AddPolicy("AdminAndUserr", build =>
+    {
+        build.RequireRole("Admin");
+        build.RequireRole("User");
+    });
+});
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<RefreshTokenContext>(option =>
+{
+    option.UseInMemoryDatabase("RefreshTokenDb");
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
