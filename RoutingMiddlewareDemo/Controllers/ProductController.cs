0ac6eb6d3383beb343d5be92012d5301f7575ea5
sushil kumar <sushilkumar.kumar29@gmail.com>
2025-01-12 22:11:50 +0530

    Implement Attribute Routing in .Net Core

--- RoutingMiddlewareDemo/Controllers/ProductController.cs
+++ RoutingMiddlewareDemo/Controllers/ProductController.cs
@@ -2,11 +2,21 @@
 
 namespace RoutingMiddlewareDemo.Controllers
 {
+    
     public class ProductController : Controller
     {
-        public IActionResult AddProduct(int id)
+        [Route("AddProduct")]
+        public IActionResult AddProduct()
         {
             return View();
+        }
+
+        [HttpPost]
+        [Route("InsertProduct")]
+        
+        public IActionResult AddProduct([FromBody] Products product)
+        {
+            return Ok(new { message = "Product has been added successfully" });
         }
     }
 }
---
+++ RoutingMiddlewareDemo/Products.cs
@@ -0,0 +1,8 @@
+﻿namespace RoutingMiddlewareDemo
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+    }
+}
